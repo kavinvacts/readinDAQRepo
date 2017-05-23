69ab0f4eafea2ffd69574e5b1e4506f2dd3e9dad
KV <kavin.varunadevan@magna.com>
2017-05-23 10:18:29 +0200

    FirstCommit

---
+++ readingDAQProj/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace readingDAQProj
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.daqTask1Component1 = new readingDAQProj.DaqTask1Component(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.waveformGraph1 = new NationalInstruments.UI.WindowsForms.WaveformGraph();
+            this.xAxis1 = new NationalInstruments.UI.XAxis();
+            this.yAxis1 = new NationalInstruments.UI.YAxis();
+            this.waveformPlot1 = new NationalInstruments.UI.WaveformPlot();
+            ((System.ComponentModel.ISupportInitialize)(this.daqTask1Component1)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.waveformGraph1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // daqTask1Component1
+            // 
+            this.daqTask1Component1.Error += new System.EventHandler<NationalInstruments.DAQmx.ComponentModel.ErrorEventArgs>(this.daqTask1Component1_Error);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.waveformGraph1);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(304, 200);
+            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(226, 175);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Read";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // waveformGraph1
+            // 
+            this.waveformGraph1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.waveformGraph1.Location = new System.Drawing.Point(2, 2);
+            this.waveformGraph1.Name = "waveformGraph1";
+            this.waveformGraph1.Plots.AddRange(new NationalInstruments.UI.WaveformPlot[] {
+            this.waveformPlot1});
+            this.waveformGraph1.Size = new System.Drawing.Size(300, 167);
+            this.waveformGraph1.TabIndex = 1;
+            this.waveformGraph1.UseColorGenerator = true;
+            this.waveformGraph1.XAxes.AddRange(new NationalInstruments.UI.XAxis[] {
+            this.xAxis1});
+            this.waveformGraph1.YAxes.AddRange(new NationalInstruments.UI.YAxis[] {
+            this.yAxis1});
+            // 
+            // waveformPlot1
+            // 
+            this.waveformPlot1.XAxis = this.xAxis1;
+            this.waveformPlot1.YAxis = this.yAxis1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(467, 404);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.daqTask1Component1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.waveformGraph1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DaqTask1Component daqTask1Component1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private NationalInstruments.UI.WindowsForms.WaveformGraph waveformGraph1;
+        private NationalInstruments.UI.WaveformPlot waveformPlot1;
+        private NationalInstruments.UI.XAxis xAxis1;
+        private NationalInstruments.UI.YAxis yAxis1;
+    }
+}
+
---
+++ readingDAQProj/Form1.cs
@@ -0,0 +1,55 @@
+﻿using NationalInstruments.Analysis;
+using NationalInstruments.Analysis.Conversion;
+using NationalInstruments.Analysis.Dsp;
+using NationalInstruments.Analysis.Dsp.Filters;
+using NationalInstruments.Analysis.Math;
+using NationalInstruments.Analysis.Monitoring;
+using NationalInstruments.Analysis.SignalGeneration;
+using NationalInstruments.Analysis.SpectralMeasurements;
+using NationalInstruments;
+using NationalInstruments.UI;
+using NationalInstruments.DAQmx;
+using NationalInstruments.NetworkVariable;
+using NationalInstruments.NetworkVariable.WindowsForms;
+using NationalInstruments.Tdms;
+using NationalInstruments.UI.WindowsForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace readingDAQProj
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                NationalInstruments.AnalogWaveform<double>[] acquiredData = daqTask1Component1.Read();
+                waveformGraph1.PlotWaveforms(acquiredData);
+            }
+            catch (NationalInstruments.DAQmx.DaqException ex)
+            {
+                MessageBox.Show(ex.Message, "DAQ Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void daqTask1Component1_Error(object sender, NationalInstruments.DAQmx.ComponentModel.ErrorEventArgs e)
+        {
+            // TODO: Handle DAQ errors.
+            string message = e.Exception.Message;
+            MessageBox.Show(message, "DAQ Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            daqTask1Component1.StopTask();
+        }
+    }
+}
